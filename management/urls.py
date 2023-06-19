b09fa7733e7fec693bae886b3ea93c8f23cdf411
prabeshyadav <prabeshyadav087@gmail.com>
2023-06-19 13:50:36 +0545

    fixed url path

--- management/urls.py
+++ management/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.Customer, name="index"),
-    path('invoice',views.InvoiceView,name='invoice'),
+    path('invoice/',views.InvoiceView,name='invoice'),
     path('login/',views.loginPage,name='login'),
     path('logout/',views.logoutUser,name='logout'),
     path('register',views.register,name='register'),
     path('reset',views.ResetPassword,name='reset'),
     path('delete/<int:id>/', views.Delete_table,name="delete_data"),
     path('edit/<int:id>/', views.update_data,name="update"),
-    path('items',views.ItemView,name='items')
+    path('items/',views.ItemView,name='items')
     #path('',views.AddCustomer.as_view(),name='index')
 ]
